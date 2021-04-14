05bc0c542578477ae586c27d73f29d82e62de922
Jesse Park <jesapark@ucsc.edu>
2021-04-14 15:26:15 -0700

    config inti error

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
+let game = new Phaser.Game(config);
+
 let config = {
     type: Phaser.AUTO,
     width: 640,
     height: 480,
     scene: [Menu, Play]
 }
-
-let game = new Phaser.Game(config);
 
 //reserve keyboard vars
 let keyF, keyR, keyLEFT, keyRIGHT;
