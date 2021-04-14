a065eb8dfd8db2a3be6ba11061a625aab689dfc7
Jesse Park <jesapark@ucsc.edu>
2021-04-14 13:45:37 -0700

    spaceshipv2

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
+let config = {
+    type: Phaser.AUTO,
+    width: 640,
+    height: 480,
+    scene: [Menu, Play]
+}
+
 let game = new Phaser.Game(config);
 
-let config = {
-    type: Phaser.CANVAS,
-    width: 640,
-    height: 480
-}
+//reserve keyboard vars
+let keyF, keyR, keyLEFT, keyRIGHT;
+
+//set UI Sizes
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
+
