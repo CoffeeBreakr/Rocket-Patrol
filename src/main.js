93fe852914e42340f170a9b1b0ccd0213cd610a2
Jesse Park <jesapark@ucsc.edu>
2021-04-14 15:51:46 -0700

    Final ITHINK

--- src/main.js
+++ src/main.js
@@ -1,23 +1,7 @@
 let config = {
     type: Phaser.AUTO,
     width: 640,
-<<<<<<< HEAD
-    height: 480,
-    scene: [Menu, Play]
-}
-
-let game = new Phaser.Game(config);
-
-//reserve keyboard vars
-let keyF, keyR, keyLEFT, keyRIGHT;
-
-//set UI Sizes
-let borderUISize = game.config.height / 15;
-let borderPadding = borderUISize / 3;
-
-=======
     height: 480
 }
 
 let game = new Phaser.Game(config);
->>>>>>> 913e2dab4444c69fde2c72b31dfe4cf08ed855de
